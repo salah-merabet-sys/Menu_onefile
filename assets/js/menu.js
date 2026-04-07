193502ae4cb339ea132e55e3c1921d58840ac005
salah-merabet-sys <lb5samer@gmail.com>
2026-04-07 16:59:56 +0200

    Fix price formatting in menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -177,7 +177,7 @@ const Menu = (() => {
           <h3 class="card-name">${item.name}</h3>
           <p class="card-desc">${item.description}</p>
           <div class="card-footer">
-            <span class="card-price">$${Number(item.price).toFixed(2)}</span>
+            <span class="card-price">${Number(item.price).toFixed(2)}</span>
           </div>
         </div>
       </article>`;
