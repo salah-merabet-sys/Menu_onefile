f3448d6d92f92cfee2876e79698f4a644a516535
salah-merabet-sys <lb5samer@gmail.com>
2026-04-07 17:01:00 +0200

    Fix price formatting in admin item display

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -244,7 +244,7 @@ const Admin = (() => {
         <div class="admin-item-info">
           <div class="admin-item-name">${item.name}</div>
           <div class="admin-item-meta">
-            <span class="admin-item-price">$${Number(item.price).toFixed(2)}</span>
+            <span class="admin-item-price">${Number(item.price).toFixed(2)}</span>
             <span>${catName}</span>
             ${tagHTML}
             <span class="stock-badge ${item.available ? 'stock-in' : 'stock-out'}">
